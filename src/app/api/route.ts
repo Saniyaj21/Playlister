cb1b16836c79324f0056d122fef37c6f419616a3
saniyaj mallik <saniyajmallik12@gmail.com>
2024-04-18 22:17:05 +0530

    deploy

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -4,14 +4,8 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
 	connectDB();
-    const user = await User.create({
-        name:'saniyaj2',
-        email: 'saniyaj2@gmail.com',
-        password: '12345678'
-    })
-	const users = await User.find();
+
 	return NextResponse.json({
 		message: "Sever is Healty.",
-		users,
 	});
 }
